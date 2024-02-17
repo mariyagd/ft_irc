bb407f711ecc36e3ff7469d238afc0e525acd47c
mariyagd <109855801+mariyagd@users.noreply.github.com>
2024-02-17 18:08:34 +0100

    ^D \n fixed

--- Server.cpp
+++ Server.cpp
@@ -138,11 +138,11 @@ void	Server::socket()
 	_servSock = ::socket( PF_INET, SOCK_STREAM, 0 );
 	if (_servSock < 0)
 	{
-		std::string error_msg = "Socket error: " + std::string( strerror(errno) );
+		std::string error_msg = "Server socket error: " + std::string( strerror(errno) );
 		throw ServerException( error_msg.c_str() );
 	}
 
-	std::cout << "Socket fd " << _servSock << " created successfully " << std::endl;
+	std::cout << "Server socket fd " << _servSock << " created successfully " << std::endl;
 }
 
 // Bind ---------------------------------------------------------------------------------------------------------------
@@ -162,7 +162,7 @@ void	Server::bind()
 		throw ServerException( error_msg.c_str() );
 	}
 
-	std::cout	<< "Socket fd " << _servSock << " bound successfully."
+	std::cout	<< "Server socket fd " << _servSock << " bound successfully."
 				<< " Familly "
 				<< getProtocolFamilyName(_server_address.sin_family)
 				<< " Port "
@@ -183,7 +183,7 @@ void	Server::listen()
 		throw ServerException( error_msg.c_str() );
 	}
 
-	std::cout << "Socket fd " << _servSock << " is listening" << std::endl;
+	std::cout << "Server socket fd " << _servSock << " is listening" << std::endl;
 }
 
 
@@ -208,8 +208,7 @@ void	Server::accept()
 	int				ret_val, i;
 	const int		DATA_BUFFER =  5000;
 	char			buf[DATA_BUFFER];
-	char *			keep_buf = NULL;
-	std::string 	result = "";
+	std::string 	result = "";																// <------------------- for ^D
 
 
 	/* Initialize all_connections and set the first entry to server fd */
@@ -219,7 +218,7 @@ void	Server::accept()
 	}
 	all_connections[0] = _servSock;
 
-	while ( _shutdown_server == false )		// <------------------------ while there no SIGINT
+	while ( _shutdown_server == false )															// <----- while there no SIGINT
 	{
 		FD_ZERO( &read_fd_set );
 		/* Set the fd_set before passing it to the select call */
@@ -231,24 +230,19 @@ void	Server::accept()
 			}
 		}
 
-		/* Invoke select() and then wait! */
-		printf("\nUsing select() to listen for incoming events\n");
-
 		ret_val = ::select( FD_SETSIZE, &read_fd_set, NULL, NULL, NULL );
 
 		/* select() woke up. Identify the fd that has events */
 		if (ret_val >= 0 )
 		{
-			printf("Select returned with %d\n", ret_val);
 			/* Check if the fd with event is the server fd */
 			if ( FD_ISSET( _servSock, &read_fd_set ) )
 			{
 				/* accept the new connection */
-				printf("Returned fd is %d (server's fd)\n", _servSock);
 				new_fd = ::accept( _servSock, ( struct sockaddr * )&new_addr, &addrlen );
 				if (new_fd >= 0)
 				{
-					printf("Accepted a new connection with fd: %d\n", new_fd);
+					std::cout << "Server accepted a new connection with fd: " << new_fd << std::endl;
 					for ( i = 0; i < MAX_CONNECTIONS; i++ )
 					{
 						if (all_connections[i] < 0)
@@ -260,7 +254,7 @@ void	Server::accept()
 				}
 				else
 				{
-					fprintf(stderr, "accept failed [%s]\n", strerror(errno));
+					std::cerr << "Accept failed: " << strerror(errno) << std::endl;
 				}
 				ret_val--;
 				if ( !ret_val ) continue;
@@ -272,65 +266,49 @@ void	Server::accept()
 				if ( ( all_connections[i] > 0 ) && ( FD_ISSET( all_connections[i], &read_fd_set ) ) )
 				{
 					/* read incoming data */
-					printf("Returned fd is %d [index, i: %d]\n", all_connections[i], i);
+
 					ret_val = recv( all_connections[i], buf, DATA_BUFFER, 0 );
 					if ( ret_val == 0 )
 					{
-						printf("Closing connection for fd:%d\n", all_connections[i]);
+						std::cout << "Connection " << all_connections[i] << " closed by client" << std::endl;
 						close( all_connections[i] );
 						all_connections[i] = -1; /* Connection is now closed */
 					}
 					if (ret_val > 0)
 					{
 						buf[ret_val] = '\0';
+						std::cout << "Received data \"" << buf << "\" from fd " << all_connections[i] << " ( len " << strlen(buf) << ")" << std::endl;
 
 						if ( strchr( buf, '\n' ) == NULL )
 						{
-							std::cout << "Data received. Found newline for ^D. Keep data for reconstitution " << std::endl;
-							if (!keep_buf)
-							{
-								keep_buf = strdup( buf );
-							}
-							else
-							{
-								char *tmp = strdup( keep_buf );
-								strcat( tmp, buf );
-								free( keep_buf );
-								keep_buf = strdup( tmp );
-								free( tmp );
-								tmp = NULL;
-							}
-							std::cout << "Keep in memory data \"" << keep_buf << "\"" << std::endl;
-
+							std::cout << "Keep data in memory" << std::endl;
+							result += std::string( buf );
 						}
 						else
 						{
-							std::cout << "Data received. No newline found. Checking if there is data in memory." << std::endl;
-
-							if ( keep_buf && buf[0] != '\0' )
+							std::cout << "Data is ready to output" << std::endl;
+							if ( result.empty() )
 							{
-								std::cout << "Found data in memory. Reconstitution process" << std::endl;
-								result = std::string(keep_buf);
+								result = std::string( buf );
+//								size_t pos = result.find( '\n' );				// <----------------------- to erase \n
+//								result.erase(pos, 1);
+
 							}
 							else
 							{
-								result = std::string( buf );
+								if ( strlen(buf) > 0 )
+									result += std::string( buf );
 							}
-							std::cout << "Data (len " << ret_val << " bytes, fd: " << all_connections[i] << "): " << result << std::endl;
+							std::cout << "Output data: (len " << result.size() << ") \"" << result << "\"" << std::endl;
 							if ( result == "exit\n" )
 								return;
-							if ( keep_buf )
-							{
-								free( keep_buf );
-								keep_buf = NULL;
-							}
 							result.clear();
 						}
 
 					}
 					if (ret_val == -1)
 					{
-						printf("recv() failed for fd: %d [%s]\n", all_connections[i], strerror(errno));
+						std::cerr << "recv() failed for fd: " << all_connections[i] << ": " << strerror(errno) << std::endl;
 						break;
 					}
 				}
